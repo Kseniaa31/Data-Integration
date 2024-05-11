30fc52928e566d8bde9932d9e6ae1a707a33a57a
kledi haxhimali <kledi.haxhimali@gmail.com>
2024-05-12 01:22:54 +0200

    Update Jaccard.java
    Jaccard

--- src/main/java/de/di/similarity_measures/Jaccard.java
+++ src/main/java/de/di/similarity_measures/Jaccard.java
@@ -19,6 +19,7 @@ public class Jaccard implements SimilarityMeasure {
      * Calculates the Jaccard similarity of the two input strings. Note that the Jaccard similarity may use set or
      * multiset, i.e., bag semantics for the union and intersect operations. The maximum Jaccard similarity with
      * multiset semantics is 1/2 and the maximum Jaccard similarity with set semantics is 1.
+     *
      * @param string1 The first string argument for the similarity calculation.
      * @param string2 The second string argument for the similarity calculation.
      * @return The multiset Jaccard similarity of the two arguments.
@@ -37,6 +38,7 @@ public class Jaccard implements SimilarityMeasure {
      * Calculates the Jaccard similarity of the two string lists. Note that the Jaccard similarity may use set or
      * multiset, i.e., bag semantics for the union and intersect operations. The maximum Jaccard similarity with
      * multiset semantics is 1/2 and the maximum Jaccard similarity with set semantics is 1.
+     *
      * @param strings1 The first string list argument for the similarity calculation.
      * @param strings2 The second string list argument for the similarity calculation.
      * @return The multiset Jaccard similarity of the two arguments.
@@ -46,66 +48,64 @@ public class Jaccard implements SimilarityMeasure {
 
     public double calculate(String[] strings1, String[] strings2) {
         double jaccardSimilarity = 0;
+        double jaccardSimilarity1=0;
         Set<String> set_string1, set_string2;
-        int union_size=0;
-        int intersection_size=0;
+        int union_size = 0;
+        int intersection_size = 0;
 
-        if(!bagSemantics){ //it means we have set semantics
-            set_string1=new HashSet<>(Arrays.asList(strings1)); //create set so that no duplicates are there
-            set_string2=new HashSet<>(Arrays.asList(strings2));
-            Set<String> intersection=new HashSet<>(set_string1); //calculate intersection
+        if (!bagSemantics) { //it means we have set semantics
+            set_string1 = new HashSet<>(Arrays.asList(strings1)); //create set so that no duplicates are there
+            set_string2 = new HashSet<>(Arrays.asList(strings2));
+            Set<String> intersection = new HashSet<>(set_string1); //calculate intersection
             intersection.retainAll(set_string2);
-            Set<String>union=new HashSet<>(set_string1); //calculate union
+            Set<String> union = new HashSet<>(set_string1); //calculate union
             union.addAll(set_string2);
-            intersection_size=intersection.size();
-            union_size=union.size();
-
+            intersection_size = intersection.size();
+            union_size = union.size();
+            if (union_size == 0) {
+                jaccardSimilarity1 = 0;
+            } else {
+                jaccardSimilarity1 = (double) intersection_size / union_size;
+            }
         }
+        else { //bag semantics
 
-        else{ //bag semantics
-            List<String> intersection = new ArrayList<>();
-            List<String> union = new ArrayList<>();
+            int intersectionSize = 0;
+            int unionSize = 0;
 
-            // Finding intersection and union
-            for (String s1 : strings1) {
-                int countInStrings1 = countOccurrences(strings1, s1);
-                int countInStrings2 = countOccurrences(strings2, s1);
-                int minCount = Math.min(countInStrings1, countInStrings2);
-                for (int i = 0; i < minCount; i++) {
-                    intersection.add(s1);
-                }
-                int maxCount = Math.max(countInStrings1, countInStrings2);
-                for (int i = 0; i < maxCount; i++) {
-                    union.add(s1);
-                }
+// Calculate intersection
+            for (String s : strings1) {
+                int countInStrings1 = countOccurrences(strings1, s);
+                int countInStrings2 = countOccurrences(strings2, s);
+                intersectionSize += Math.min(countInStrings1, countInStrings2);
             }
 
-            for (String s2 : strings2) {
-                if (!intersection.contains(s2)) {
-                    int countInStrings1 = countOccurrences(strings1, s2);
-                    int countInStrings2 = countOccurrences(strings2, s2);
-                    int minCount = Math.min(countInStrings1, countInStrings2);
-                    for (int i = 0; i < minCount; i++) {
-                        intersection.add(s2);
-                    }
-                    int maxCount = Math.max(countInStrings1, countInStrings2);
-                    for (int i = 0; i < maxCount; i++) {
-                        union.add(s2);
-                    }
-                }
+// Calculate union
+            for (String s : strings1) {
+                int countInStrings1 = countOccurrences(strings1, s);
+                int countInStrings2 = countOccurrences(strings2, s);
+                unionSize += Math.max(countInStrings1, countInStrings2);
             }
-            union_size=union.size();
-            intersection_size=intersection.size();
+            for (String s : strings2) {
+                int countInStrings1 = countOccurrences(strings1, s);
+                int countInStrings2 = countOccurrences(strings2, s);
+                unionSize += Math.max(countInStrings1, countInStrings2);
+            }
+
+            unionSize=unionSize-intersectionSize;
+
+            if (unionSize == 0) {
+                jaccardSimilarity1 = 0;
+            } else {
+                jaccardSimilarity1 = (double) intersectionSize / unionSize;
+            }
         }
-        if(union_size==0){
-            jaccardSimilarity=0;}
-        else {
-            jaccardSimilarity = (double)intersection_size / union_size;
-        }
+        jaccardSimilarity = Math.round(jaccardSimilarity1 * 1000000.0) / 1000000.0;
         return jaccardSimilarity;
     }
 
-    private int countOccurrences(String[] array, String element) {
+
+    private static int countOccurrences(String[] array, String element) {
         int count = 0;
         for (String s : array) {
             if (s.equals(element)) {
@@ -114,8 +114,8 @@ public class Jaccard implements SimilarityMeasure {
         }
         return count;
     }
-
 }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                      DATA INTEGRATION ASSIGNMENT                                           //
 // Calculate the Jaccard similarity of the two String arrays. Note that the Jaccard similarity needs to be    //
